992eb315e16e9c9d7fa7d8358501b49a342e0984
deepak <deepak_dit2009@outlook.com>
2022-02-25 13:39:27 -0500

    Added route table, nat and internet gateway

--- Networking/BasicNetworkingInfra/main.tf
+++ Networking/BasicNetworkingInfra/main.tf
@@ -111,3 +111,93 @@ resource "aws_subnet" "DbC" {
     By="terraform"
   }
 }
+
+locals {
+  subnets_public = [aws_subnet.publicA.id , aws_subnet.publicB.id , aws_subnet.publicC.id]
+  subnets_private_app = [aws_subnet.AppA,aws_subnet.AppB,aws_subnet.AppC]
+  subnets_private_db = [aws_subnet.DbA , aws_subnet.DbB , aws_subnet.DbC]
+}
+
+resource "aws_internet_gateway" "manning_koffee_MyIGW" {
+  vpc_id = aws_vpc.koffee_vpc.id
+  tags = {
+    Name = "${var.namespace}-MyIGW"
+    For = var.namespace
+    By="terraform"
+  }
+}
+
+resource "aws_eip" "manning_koffee_eip" {
+  count = 3
+  network_border_group = "us-east-1"
+  tags = {
+    Name = "${var.namespace}-eip-${count.index}"
+    For = var.namespace
+    By="terraform"
+  }
+  depends_on = [aws_internet_gateway.manning_koffee_MyIGW]
+}
+
+resource "aws_nat_gateway" "manning_koffee_nat_gateway" {
+  count = length(local.subnets_public)
+  subnet_id = local.subnets_public[count.index]
+  allocation_id = aws_eip.manning_koffee_eip[count.index].id
+  depends_on = [aws_eip.manning_koffee_eip]
+}
+
+resource "aws_route_table" "manning_koffee_route_table_public" {
+  vpc_id = aws_vpc.koffee_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.manning_koffee_MyIGW.id
+  }
+
+  tags = {
+    Name = "${var.namespace}-route-table-public-routetable"
+    For = var.namespace
+    By="terraform"
+  }
+  depends_on = [aws_internet_gateway.manning_koffee_MyIGW]
+}
+
+
+
+resource "aws_route_table_association" "manning_koffee_route_association_public" {
+  count = length(local.subnets_public)
+  route_table_id = aws_route_table.manning_koffee_route_table_public.id
+  subnet_id = local.subnets_public[count.index]
+}
+
+
+resource "aws_route_table" "manning_koffee_route_table_private" {
+  count = length(aws_nat_gateway.manning_koffee_nat_gateway)
+  vpc_id = aws_vpc.koffee_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.manning_koffee_nat_gateway[count.index].id
+  }
+
+  tags = {
+    Name = "${var.namespace}-route-table-private-${count.index}"
+    For = var.namespace
+    By="terraform"
+  }
+  depends_on = [aws_nat_gateway.manning_koffee_nat_gateway]
+}
+
+
+resource "aws_route_table_association" "manning_koffee_route_association_private_app" {
+  count = length(aws_route_table.manning_koffee_route_table_private)
+  route_table_id = aws_route_table.manning_koffee_route_table_private[count.index].id
+  subnet_id = local.subnets_private_app[count.index].id
+  depends_on = [aws_route_table.manning_koffee_route_table_private]
+}
+
+resource "aws_route_table_association" "manning_koffee_route_association_private_db" {
+  count = length(aws_route_table.manning_koffee_route_table_private)
+  route_table_id = aws_route_table.manning_koffee_route_table_private[count.index].id
+  subnet_id = local.subnets_private_db[count.index].id
+  depends_on = [aws_route_table.manning_koffee_route_table_private]
+}
